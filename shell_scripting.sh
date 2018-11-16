14b835c71ef4cdf0f065208b297c4f242137d656
katyhannath <katyhannath@yahoo.co.uk>
2018-11-16 16:44:50 +0000

    my first commit

--- shell_scripting.sh
+++ shell_scripting.sh
@@ -2,21 +2,21 @@
 #update date daily at 6:00 AM
 #Author: Solomon Amos
 
-DATE=`date "+%d-%m-%Y %a %p %l:%M %Z"`
-Weather=$(curl wttr.in/?0?q?T | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$0}')
-time=$(date +%d-%m-%Y).png
+ DATE=`date "+%d-%m-%Y %a %p %l:%M %Z"`
+ Weather=$(curl wttr.in/?0?q?T | awk '/°(C|F)/ {printf $(NF-1) $(NF) " ("a")"} /,/ {a=$0}')
+ time=$(date +%d-%m-%Y).png
 
-echo -e "* $DATE   : $Weather" | tee -a ~/projects/shell_scripting/README.md
+ echo -e "* $DATE   : $Weather" | tee -a ~/shell_scripting/README.md
 
-date +%d-%m-%Y >> ~/projects/shell_scripting/weather.md
+ date +%d-%m-%Y >> ~/shell_scripting/weather.md
 
-curl -L 'wttr.in/York=.png' -o ~/projects/shell_scripting/img/$time
+ curl -L 'wttr.in/York=.png' -o ~/shell_scripting/img/$time
  
-echo -e "\n![](img/$time)\n" >> ~/projects/shell_scripting/weather.md
+ echo -e "\n![](img/$time)\n" >> ~/shell_scripting/weather.md
 
-cd ~/projects/shell_scripting
-git init
-git add .
-git commit -m "update Today's log:)"
-# git remote add origin https://github.com/udohsolomon/shell_scripting.git
-git push -f origin master
+ cd ~/shell_scripting
+ git init
+ git add .
+ git commit -m "my first commit"
+ git remote add origin https://github.com/katyhannath/shell_script.git
+ #git push -u origin master
